5f0a7499cd9d19fdd382893981197d4235dd834b
Vaibhav Pujari <vaibhav276@yahoo.co.in>
2018-05-29 15:53:22 +0530

    Basic version works

--- app.js
+++ app.js
@@ -1,22 +1,49 @@
+$(function() {
+   var p;
+   var it;
+   var mutRate;
+   $('#status').html("Ready");
+   $("#startIterator").click(function() {
+      var target1 = $("#target").val();
+      var popSize = $("#population-size").val();
+      mutRate = $("#mutation-rate").val();
 
-console.log("Loaded");
+      p = randomPopulation(target1, popSize);
+      $("#generation").html(p.generation);
+      $("#best-gene").html(topGene(p).code)
+      refreshPopulationTable(p);
+      $('#status').html("Started");
+      it = setInterval(iterate, 1);
+   });
 
-var x = new Gene();
-var y = new Gene();
-x.newRandom(20);
-y.newRandom(20);
+   $("#stopIterator").click(function() {
+      $('#status').html("Paused");
+      clearInterval(it);
+   });
 
-console.log("x = " + x.code);
-console.log("y = " + y.code);
+   function iterate() {
+      if (topGene(p).cost == 0) {
+         $('#status').html("Done");
+         clearInterval(it);
+      }
+      var p1 = evolve(p, mutRate);
+      var p2 = score(p1)
+      $("#generation").html(p2.generation);
+      $("#best-gene").html(topGene(p2).code)
+      refreshPopulationTable(p2);
+      p = p2;
+   }
 
-x.calcCost(y);
+   function refreshPopulationTable(pop) {
+      $("#population-table tbody tr").remove();
+      for (var i = 0; i < pop.elements.length; i++) {
+         $("#population-table").append("<tr><td>" +
+                                       pop.elements[i].code.join('') +
+                                       "</td><td>" +
+                                       pop.elements[i].cost +
+                                       "</td></tr>"
+                                      );
+      }
+   }
 
-console.log("x/y cost = " + x.cost);
-
-console.log("child = " + x.mate(y).code);
-
-var p = new Population(new Gene("hello"), 20);
-console.log(p);
-p.score();
-console.log(p);
-$("#population").html(p.asHtml());
+});
--- lib.js
+++ lib.js
@@ -6,76 +6,143 @@ function getRandomInt(max) {
    return Math.floor(Math.random() * Math.floor(max));
 }
 
-function diff(a,b) {
-   return a.charCodeAt(0) - b.charCodeAt(0);
+/** Squared error between two char codes
+ */
+function charSqError(a,b) {
+   return (a.charCodeAt(0) - b.charCodeAt(0))*(a.charCodeAt(0) - b.charCodeAt(0));
 }
 
-var Gene = function(code) {
-   if (code) this.code = code;
+/********** Gene functions ************/
+var Gene = function() {
+   this.code = []
+   this.score = 0.0;
    this.cost = 9999;
-   this.score = 0;
 }
-Gene.prototype.code = '';
-Gene.prototype.newRandom = function(length) {
-   this.code = _.sample(dnaCodes, length);
+/** Generate a random gene sequence */
+function randomSequence(length) {
+   return _.sample(dnaCodes, length);
 }
-Gene.prototype.maybeMutate = function(chance) {
-   if (Math.random() > chance) return;
+/** Mutate, or not */
+function maybeMutate(gene, chance) {
+   if (Math.random() > chance) return gene;
 
-   var index = getRandomInt(this.code.length);
-   var newCode = dnaCodes[getRandomInt(dnaCodes.length)];
-   this.code[index] = newCode;
-   return this;
+   var index = getRandomInt(gene.code.length);
+   var direction = Math.random() > 0.5 ? 1 : -1;
+   var dnaCodesIndex = dnaCodes.indexOf(gene.code[index]);
+   dnaCodesIndex += direction;
+
+   // wrap-around
+   if (dnaCodesIndex < 0) { dnaCodesIndex = dnaCodes.length - 1; }
+   if (dnaCodesIndex >= dnaCodes.length) { dnaCodesIndex = 0; }
+
+   var res = gene;
+   res.code[index] = dnaCodes[dnaCodesIndex];
+   return res;
 }
-Gene.prototype.mate = function(gene) {
-   var mid = this.code.length / 2;
-   var part1 = _.take(this.code, mid);
-   var part2 = _.drop(gene.code, mid);
-   return new Gene(part1 + part2);
+/** Basic mating algorithm
+ */
+function mate(gene1, gene2) {
+   var mid = gene1.code.length / 2;
+   var part1 = _.take(gene1.code, mid);
+   var part2 = _.drop(gene2.code, mid);
+   var res = new Gene();
+   res.code = part1.concat(part2);
+   return res;
 }
-Gene.prototype.calcCost = function(target) {
-   var zipped = _.zip(this.code, target.code);
-   var sqSum = _.foldl(zipped, function(sum, a) { return sum + (diff(a[0],a[1]))*(diff(a[0],a[1])); }, 0);
-   this.cost = sqSum;
-   return this;
+/** Cost is sum of squared errors at each position
+ */
+function calcCost(gene, target) {
+   var zipped = _.zip(gene.code, target.code);
+   var sqSum = _.foldl(zipped, function(sum, a) { return sum + (charSqError(a[0],a[1])); }, 0);
+   return sqSum;
 }
 
-
-var Population = function(target, size) {
+/************* Population functions ***************/
+var Population = function(target) {
    this.elements = [];
-   this.target = target;
+   this.target = new Gene();
+   this.target.code = target;
    this.generation = 0;
-
+}
+/** Generate a new random population
+ *  Used for initialization
+ */
+function randomPopulation(target, size) {
+   var res = new Population(target);
    while(size--) {
       var gene = new Gene();
-      gene.newRandom(target.code.length);
-      this.elements.push(gene);
+      gene.code = randomSequence(target.length);
+      res.elements.push(gene);
    }
-}
-Population.prototype.asHtml = function() {
-   var res = '';
-   res += ("<h2>Generation: " + this.generation + "</h2>");
-   res += ("<ul>");
-   for (var i = 0; i < this.elements.length; i++) {
-      res += ("<li>" + this.elements[i].code + " (" + this.elements[i].cost + ")");
-   }
-   res += ("</ul>");
    return res;
-};
-Population.prototype.sort = function() {
-   this.members.sort(function(a, b) {
-      return a.cost - b.cost;
-   });
 }
-Population.prototype.score = function() {
-   var tgt = this.target;
-   this.elements = _.map(this.elements, function(x) { return x.calcCost(tgt); })
-   var minCost = _.min(this.elements, function(x) { return x.cost; });
-   var maxCost = _.max(this.elements, function(x) { return x.cost; });
+/** Calculate normalized (0-1) score from cost range of population
+ */
+function score(population) {
+   var costedElements = _.map(population.elements, function(x) {
+      x.cost = calcCost(x, population.target);
+      return x;
+   });
+   var minCost = _.min(costedElements, function(x) { return x.cost; });
+   var maxCost = _.max(costedElements, function(x) { return x.cost; });
    var range = maxCost.cost - minCost.cost;
-   this.elements = _.map(this.elements, function (x) {
+   if (range == 0) range = 1; // Prevent divide by 0 - all elements are same
+   var scoredElements = _.map(population.elements, function (x) {
       x.score = 1.0 - ( (x.cost - minCost.cost) / range );
       return x;
    });
-   return this;
+
+   var res = population;
+   res.elements = scoredElements;
+
+   return res;
 }
+/** Pick a random parent, based on giving higher chance
+ *  to elements having higher score
+ */
+function pickParent(population) {
+   // NOTE: population must be scored already
+
+   var min = _.min(population.elements, function(x) { return x.score; });
+   var max = _.max(population.elements, function(x) { return x.score; });
+   var potentialParent = _.sample(population.elements);
+   if (min.score == max.score) {
+      // No variation
+      // Maybe initial case, or somewhere during evolution
+      return potentialParent;
+   }
+
+   while (true) {
+      // Accept-reject mechanism
+      // https://www.wikiwand.com/en/Rejection_sampling
+      if (potentialParent.score > Math.random()) {
+         return potentialParent;
+      }
+      potentialParent = _.sample(population.elements);
+   }
+}
+/** Evolve population to its next generation
+ */
+function evolve(population, mutationRate) {
+   var children = [];
+   var nextGenSize = population.elements.length;
+
+   while (children.length < nextGenSize) {
+      var p1 = pickParent(population);
+      var p2 = pickParent(population);
+      var child = maybeMutate (mate(p1, p2), mutationRate);
+      children.push(child);
+   }
+   var res = population;
+   res.generation = population.generation + 1;
+   res.elements = children;
+
+   return res;
+}
+/** Pick top gene for convergence checks
+ */
+function topGene(population) {
+   // NOTE: population must be scored already
+   return _.min(population.elements, function(x) { return x.cost; });
+}
+

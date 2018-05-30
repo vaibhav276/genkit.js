eb67f26d87ec9386a6a593061192e95036af2db5
Vaibhav Pujari <vaibhav276@yahoo.co.in>
2018-05-30 14:55:32 +0530

    Librarify

--- app.js
+++ app.js
@@ -2,16 +2,56 @@ $(function() {
    var p;
    var it;
    var mutRate;
+
+   // The dna codes for our case
+   var dnaCodes = 'abcdefghijklmnopqrstuvwxyz          ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+
+   /** Distance between two dna codes
+    *  is the square of their ascii value difference
+    */
+   function error(a,b) {
+      return (a.charCodeAt(0) - b.charCodeAt(0))*(a.charCodeAt(0) - b.charCodeAt(0));
+   }
+   /** Mutate a given dnaCode
+    *  In our case, it just moves either up or down (randomly)
+    *  in the dna codes array
+    */
+   function mutate(dnaCode, dnaCodes) {
+      var direction = Math.random() > 0.5 ? 1 : -1;
+      var dnaCodesIndex = dnaCodes.indexOf(dnaCode);
+      dnaCodesIndex += direction;
+
+      // wrap-around
+      if (dnaCodesIndex < 0) { dnaCodesIndex = dnaCodes.length - 1; }
+      if (dnaCodesIndex >= dnaCodes.length) { dnaCodesIndex = 0; }
+      return dnaCodes[dnaCodesIndex];
+   }
+   /** Basic mating function
+    *  In our case, it just picks first half from first parent
+    *  and second half from second parent
+    */
+   function mate(str1, str2) {
+      // NOTE: str1 and str2 are of same lengths
+      var mid = str1.length / 2;
+      var part1 = _.take(str1, mid);
+      var part2 = _.drop(str2, mid);
+      return part1.concat(part2);
+   }
+
+   var gk = new GenKit({
+      dnaCodes: dnaCodes,
+      error: error,
+      mutate: mutate,
+      mate: mate
+   });
+
    $('#status').html("Ready");
    $("#startIterator").click(function() {
       var target1 = $("#target").val();
       var popSize = $("#population-size").val();
       mutRate = $("#mutation-rate").val();
 
-      p = randomPopulation(target1, popSize);
-      $("#generation").html(p.generation);
-      $("#best-gene").html(topGene(p).code)
-      refreshPopulationTable(p);
+      p = gk.randomPopulation(target1, popSize);
       $('#status').html("Started");
       it = setInterval(iterate, 1);
    });
@@ -22,16 +62,19 @@ $(function() {
    });
 
    function iterate() {
-      if (topGene(p).cost == 0) {
+      var topGene = gk.topGene(p);
+      $("#generation").html(p.generation);
+      $("#best-gene").html(topGene.code);
+      $("#best-gene-cost").html(topGene.cost);
+      refreshPopulationTable(p);
+
+      if (topGene.cost == 0) {
          $('#status').html("Done");
          clearInterval(it);
       }
-      var p1 = evolve(p, mutRate);
-      var p2 = score(p1)
-      $("#generation").html(p2.generation);
-      $("#best-gene").html(topGene(p2).code)
-      refreshPopulationTable(p2);
-      p = p2;
+      var p1 = gk.evolve(p, mutRate);
+      p1 = gk.score(p1);
+      p = p1;
    }
 
    function refreshPopulationTable(pop) {
--- lib.js
+++ lib.js
@@ -1,148 +1,145 @@
 /* Requires: underscore.js */
 
-var dnaCodes = 'abcdefghijklmnopqrstuvwxyz          ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+var GenKit = function(config) {
 
-function getRandomInt(max) {
-   return Math.floor(Math.random() * Math.floor(max));
-}
+   this.dnaCodes = config.dnaCodes; // The dna codes
+   this.error = config.error; // The error function to tell us distance between two dna codes
+   this.mutate = config.mutate; // Mutation function for a dnaCode
+   this.mate = config.mate;
+   var theObj = this;
 
-/** Squared error between two char codes
- */
-function charSqError(a,b) {
-   return (a.charCodeAt(0) - b.charCodeAt(0))*(a.charCodeAt(0) - b.charCodeAt(0));
-}
-
-/********** Gene functions ************/
-var Gene = function() {
-   this.code = []
-   this.score = 0.0;
-   this.cost = 9999;
-}
-/** Generate a random gene sequence */
-function randomSequence(length) {
-   return _.sample(dnaCodes, length);
-}
-/** Mutate, or not */
-function maybeMutate(gene, chance) {
-   if (Math.random() > chance) return gene;
-
-   var index = getRandomInt(gene.code.length);
-   var direction = Math.random() > 0.5 ? 1 : -1;
-   var dnaCodesIndex = dnaCodes.indexOf(gene.code[index]);
-   dnaCodesIndex += direction;
-
-   // wrap-around
-   if (dnaCodesIndex < 0) { dnaCodesIndex = dnaCodes.length - 1; }
-   if (dnaCodesIndex >= dnaCodes.length) { dnaCodesIndex = 0; }
-
-   var res = gene;
-   res.code[index] = dnaCodes[dnaCodesIndex];
-   return res;
-}
-/** Basic mating algorithm
- */
-function mate(gene1, gene2) {
-   var mid = gene1.code.length / 2;
-   var part1 = _.take(gene1.code, mid);
-   var part2 = _.drop(gene2.code, mid);
-   var res = new Gene();
-   res.code = part1.concat(part2);
-   return res;
-}
-/** Cost is sum of squared errors at each position
- */
-function calcCost(gene, target) {
-   var zipped = _.zip(gene.code, target.code);
-   var sqSum = _.foldl(zipped, function(sum, a) { return sum + (charSqError(a[0],a[1])); }, 0);
-   return sqSum;
-}
-
-/************* Population functions ***************/
-var Population = function(target) {
-   this.elements = [];
-   this.target = new Gene();
-   this.target.code = target;
-   this.generation = 0;
-}
-/** Generate a new random population
- *  Used for initialization
- */
-function randomPopulation(target, size) {
-   var res = new Population(target);
-   while(size--) {
-      var gene = new Gene();
-      gene.code = randomSequence(target.length);
-      res.elements.push(gene);
-   }
-   return res;
-}
-/** Calculate normalized (0-1) score from cost range of population
- */
-function score(population) {
-   var costedElements = _.map(population.elements, function(x) {
-      x.cost = calcCost(x, population.target);
-      return x;
-   });
-   var minCost = _.min(costedElements, function(x) { return x.cost; });
-   var maxCost = _.max(costedElements, function(x) { return x.cost; });
-   var range = maxCost.cost - minCost.cost;
-   if (range == 0) range = 1; // Prevent divide by 0 - all elements are same
-   var scoredElements = _.map(population.elements, function (x) {
-      x.score = 1.0 - ( (x.cost - minCost.cost) / range );
-      return x;
-   });
-
-   var res = population;
-   res.elements = scoredElements;
-
-   return res;
-}
-/** Pick a random parent, based on giving higher chance
- *  to elements having higher score
- */
-function pickParent(population) {
-   // NOTE: population must be scored already
-
-   var min = _.min(population.elements, function(x) { return x.score; });
-   var max = _.max(population.elements, function(x) { return x.score; });
-   var potentialParent = _.sample(population.elements);
-   if (min.score == max.score) {
-      // No variation
-      // Maybe initial case, or somewhere during evolution
-      return potentialParent;
+   function getRandomInt(max) {
+      return Math.floor(Math.random() * Math.floor(max));
    }
 
-   while (true) {
-      // Accept-reject mechanism
-      // https://www.wikiwand.com/en/Rejection_sampling
-      if (potentialParent.score > Math.random()) {
+   /********** Gene functions ************/
+   var Gene = function() {
+      this.code = [];
+      this.score = 0.0;
+      this.cost = Number.MAX_SAFE_INTEGER;
+   };
+   /** Generate a random gene sequence */
+   function randomDnaSequence(length) {
+      return _.sample(theObj.dnaCodes, length);
+   }
+   /** Mutate, or not */
+   function maybeMutate(gene, chance) {
+      if (Math.random() > chance) return gene;
+
+      var index = getRandomInt(gene.code.length);
+      var res = gene;
+      res.code[index] = theObj.mutate(res.code[index], theObj.dnaCodes);
+      return res;
+   }
+   /** Mate two genes
+    */
+   function mateWrapper(gene1, gene2) {
+      var res = new Gene();
+      res.code = theObj.mate(gene1.code, gene2.code);
+      return res;
+   }
+   /** Cost is sum of errors at each position
+    */
+   function calcCost(gene, target) {
+      var zipped = _.zip(gene.code, target.code);
+      return _.foldl(zipped, function(sum, a) {
+         return sum + (theObj.error(a[0],a[1]));
+      }, 0);
+   }
+
+   /************* Population functions ***************/
+   var Population = function(target) {
+      this.elements = [];
+      this.target = new Gene();
+      this.target.code = target;
+      this.generation = 0;
+   }
+   /** Generate a new random population
+    *  Used for initialization
+    */
+   function randomPopulation(target, size) {
+      var res = new Population(target);
+      while(size--) {
+         var gene = new Gene();
+         gene.code = randomDnaSequence(target.length);
+         res.elements.push(gene);
+      }
+      return res;
+   }
+   /** Calculate normalized (0-1) score from cost range of population
+    */
+   function score(population) {
+      var costedElements = _.map(population.elements, function(x) {
+         x.cost = calcCost(x, population.target);
+         return x;
+      });
+      var minCost = _.min(costedElements, function(x) { return x.cost; });
+      var maxCost = _.max(costedElements, function(x) { return x.cost; });
+      var range = maxCost.cost - minCost.cost;
+      if (range == 0) range = 1; // Prevent divide by 0 - all elements are same
+      var scoredElements = _.map(population.elements, function (x) {
+         x.score = 1.0 - ( (x.cost - minCost.cost) / range );
+         return x;
+      });
+
+      var res = population;
+      res.elements = scoredElements;
+
+      return res;
+   }
+   /** Pick a random parent, based on giving higher chance
+    *  to elements having higher score
+    */
+   function pickParent(population) {
+      // NOTE: population must be scored already
+
+      var min = _.min(population.elements, function(x) { return x.score; });
+      var max = _.max(population.elements, function(x) { return x.score; });
+      var potentialParent = _.sample(population.elements);
+      if (min.score == max.score) {
+         // No variation
+         // Maybe initial case, or somewhere during evolution
          return potentialParent;
       }
-      potentialParent = _.sample(population.elements);
+
+      // Accept-reject mechanism
+      // https://www.wikiwand.com/en/Rejection_sampling
+      while (true) {
+         if (potentialParent.score > Math.random()) {
+            return potentialParent;
+         }
+         potentialParent = _.sample(population.elements);
+      }
    }
-}
-/** Evolve population to its next generation
- */
-function evolve(population, mutationRate) {
-   var children = [];
-   var nextGenSize = population.elements.length;
+   /** Evolve population to its next generation
+    */
+   function evolve(population, mutationRate) {
+      var children = [];
+      var nextGenSize = population.elements.length;
 
-   while (children.length < nextGenSize) {
-      var p1 = pickParent(population);
-      var p2 = pickParent(population);
-      var child = maybeMutate (mate(p1, p2), mutationRate);
-      children.push(child);
+      while (children.length < nextGenSize) {
+         var p1 = pickParent(population);
+         var p2 = pickParent(population);
+         var child = maybeMutate (mateWrapper(p1, p2), mutationRate);
+         children.push(child);
+      }
+      var res = population;
+      res.generation = population.generation + 1;
+      res.elements = children;
+
+      return res;
    }
-   var res = population;
-   res.generation = population.generation + 1;
-   res.elements = children;
+   /** Pick top gene for convergence checks
+    */
+   function topGene(population) {
+      // NOTE: population must be scored already
+      return _.min(population.elements, function(x) { return x.cost; });
+   }
 
-   return res;
+   return {
+      randomPopulation: randomPopulation,
+      evolve: evolve,
+      score: score,
+      topGene: topGene
+   };
 }
-/** Pick top gene for convergence checks
- */
-function topGene(population) {
-   // NOTE: population must be scored already
-   return _.min(population.elements, function(x) { return x.cost; });
-}
-
